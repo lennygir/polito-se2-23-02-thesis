73d97429bf4f7fb0231049f74c94110a962232bc
Lorenzo Bertetto <lorenzo.bertetto@gmail.com>
2023-11-08 09:47:12 +0100

    Added code for login endpoint. To be tested.

--- server/src/routes.js
+++ server/src/routes.js
@@ -1,13 +1,61 @@
 "use strict";
 
 const router = require("express").Router();
+const userDao = require('./user-dao');
 
 // ==================================================
 // Routes
 // ==================================================
 
 router.get("/api", (req, res) => {
-  res.status(200).json({ message: "API home page" });
+    return res.status(200).json({ message: "API home page" });
+});
+
+// login endpoint
+/*
+    {
+        "user": "s123456",
+        "password": "password"
+    }
+    returns
+    if user is a student: {
+        "role": "student"
+        "StudentID":	    Integer
+        "Surname":	        Text
+        "Name":	            Text
+        "Gender":	        Text
+        "Nationality":	    Text
+        "Email":	        Text
+        "COD_degree"	    Text
+        "Enrollment_year":	Date
+    }
+    if user is a teacher {
+        "role": "teacher"
+        "TeacherID":        integer
+        "Name":             text
+        "Surname":          text
+        "Email":            text
+        "GroupID":          text (maybe integer)
+        "DepartmentID":     text (maybe integer)
+    }
+*/
+router.post("/api/sessions", async (req, res) => {
+    const valid = await userDao.checkUser(req.body.user, req.body.password);
+    if (valid) {
+        const teacher = await userDao.getTeacher();
+        const student = await userDao.getStudent();
+        if (teacher) {
+            teacher.role = "teacher";
+            return res.status(200).send(teacher);
+        } else if (student) {
+            student.role = "student";
+            return res.status(200).send(student);
+        } else {
+            return res.status(500).send({ message: "Unknown error" });
+        }
+    } else {
+        return res.status(404).send({ message: "Invalid login credentials" });
+    }
 });
 
 
---
+++ server/src/user-dao.js
@@ -0,0 +1,51 @@
+'use strict';
+
+/* Data Access Object (DAO) module for accessing users */
+
+const {db} = require('./db');
+
+// FAKE AUTHENTICATION. MUST BE SUBSTITUTED BY SAML2.0
+exports.checkUser = (id, password) => {
+    return new Promise((resolve, reject) => {
+        const sql = 'SELECT * FROM users WHERE loginID = ?';
+        db.get(sql, [id], (err,row) => {
+            if (err) { // sql error
+                reject(err);
+            } else if (row === undefined) { // there is not a user with this ID
+                resolve(false);
+            } else {
+                resolve(row.password === password);
+            }
+        })
+    })
+}
+
+exports.getTeacher = (id) => {
+    return new Promise((resolve, reject) => {
+        const sql = 'SELECT * FROM teachers WHERE TeacherID = ?';
+        db.get(sql, [id], (err,row) => {
+            if (err) {
+                reject(err);
+            } else if (row === undefined) { // the id is not a teacherID
+                resolve(false);
+            } else {
+                resolve(row);
+            }
+        })
+    })
+}
+
+exports.getStudent = (id) => {
+    return new Promise((resolve, reject) => {
+        const sql = 'SELECT * FROM students WHERE StudentID = ?';
+        db.get(sql, [id], (err,row) => {
+            if (err) {
+                reject(err);
+            } else if (row === undefined) { // the id is not a studentID
+                resolve(false);
+            } else {
+                resolve(row);
+            }
+        })
+    })
+}
