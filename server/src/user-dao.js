2f156034f2cc92043779c77fae51b8c37e68a904
Lorenzo Bertetto <lorenzo.bertetto@gmail.com>
2023-11-11 13:09:46 +0100

    Login endpoint completed.

--- server/src/user-dao.js
+++ server/src/user-dao.js
@@ -1,51 +1,55 @@
-'use strict';
+"use strict";
 
 /* Data Access Object (DAO) module for accessing users */
 
-const {db} = require('./db');
+const { db } = require("./db");
 
-// FAKE AUTHENTICATION. MUST BE SUBSTITUTED BY SAML2.0
-exports.checkUser = (id, password) => {
-    return new Promise((resolve, reject) => {
-        const sql = 'SELECT * FROM users WHERE loginID = ?';
-        db.get(sql, [id], (err,row) => {
-            if (err) { // sql error
-                reject(err);
-            } else if (row === undefined) { // there is not a user with this ID
-                resolve(false);
-            } else {
-                resolve(row.password === password);
-            }
-        })
-    })
-}
+// todo: FAKE AUTHENTICATION. MUST BE SUBSTITUTED BY SAML2.0
+exports.checkUser = (email, password) => {
+  return new Promise((resolve, reject) => {
+    const sql = "SELECT * FROM users WHERE email = ?";
+    db.get(sql, [email], (err, row) => {
+      if (err) {
+        // sql error
+        reject(err);
+      } else if (row === undefined) {
+        // there is not a user with this ID
+        resolve(false);
+      } else {
+        resolve(row.password === password);
+      }
+    });
+  });
+};
 
 exports.getTeacher = (id) => {
-    return new Promise((resolve, reject) => {
-        const sql = 'SELECT * FROM teachers WHERE TeacherID = ?';
-        db.get(sql, [id], (err,row) => {
-            if (err) {
-                reject(err);
-            } else if (row === undefined) { // the id is not a teacherID
-                resolve(false);
-            } else {
-                resolve(row);
-            }
-        })
-    })
-}
+  return new Promise((resolve, reject) => {
+    const sql = "SELECT * FROM teacher WHERE email = ?";
+    db.get(sql, [id], (err, row) => {
+      if (err) {
+        reject(err);
+      } else if (row === undefined) {
+        // the id is not a teacherID
+        resolve(false);
+      } else {
+        resolve(row);
+      }
+    });
+  });
+};
 
 exports.getStudent = (id) => {
-    return new Promise((resolve, reject) => {
-        const sql = 'SELECT * FROM students WHERE StudentID = ?';
-        db.get(sql, [id], (err,row) => {
-            if (err) {
-                reject(err);
-            } else if (row === undefined) { // the id is not a studentID
-                resolve(false);
-            } else {
-                resolve(row);
-            }
-        })
-    })
-}
+  return new Promise((resolve, reject) => {
+    const sql = "SELECT * FROM student WHERE email = ?";
+    db.get(sql, [id], (err, row) => {
+      if (err) {
+        reject(err);
+      } else if (row === undefined) {
+        // the id is not a studentID
+        resolve(false);
+      } else {
+        resolve(row);
+      }
+    });
+  });
+};
