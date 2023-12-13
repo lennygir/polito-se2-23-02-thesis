cdbe78f08a33b6f7745cc267874ce4a0622a24bf
Carlos Valeriano <carlosv.2064@gmail.com>
2023-12-13 10:32:47 +0100

    code duplicated fixed

--- client/src/components/ApplicationTable.jsx
+++ client/src/components/ApplicationTable.jsx
@@ -14,33 +14,27 @@ import UserContext from "../contexts/UserContext";
 const TEACHER_HEADERS = ["Student", "Proposal", "Status", "Open"];
 const STUDENT_HEADERS = ["Teacher", "Proposal", "Status", "Open"];
 
+function generateTableHeaders(headers, align) {
+  return headers.map((headCell) => (
+    <TableCell key={headCell} align={headCell === "Status" ? "center" : align} variant="head">
+      <Typography fontWeight={700} fontSize={18}>
+        {headCell}
+      </Typography>
+    </TableCell>
+  ));
+}
+
 function ApplicationTable(props) {
   const { applications } = props;
   const user = useContext(UserContext);
+  const headers = user?.role === "student" ? STUDENT_HEADERS : TEACHER_HEADERS;
 
   return (
     <Paper sx={{ mt: { md: 3, xs: 1 }, mx: { md: 4, xs: 0 }, overflow: "hidden", borderRadius: 4 }}>
       <TableContainer sx={{ maxHeight: "60vh" }}>
         <Table stickyHeader>
           <TableHead>
-            <TableRow>
-              {user?.role === "student" &&
-                STUDENT_HEADERS.map((headCell) => (
-                  <TableCell key={headCell} align={headCell === "Status" ? "center" : "inherit"} variant="head">
-                    <Typography fontWeight={700} fontSize={18}>
-                      {headCell}
-                    </Typography>
-                  </TableCell>
-                ))}
-              {user?.role === "teacher" &&
-                TEACHER_HEADERS.map((headCell) => (
-                  <TableCell key={headCell} align={headCell === "Status" ? "center" : "inherit"} variant="head">
-                    <Typography fontWeight={700} fontSize={18}>
-                      {headCell}
-                    </Typography>
-                  </TableCell>
-                ))}
-            </TableRow>
+            <TableRow>{generateTableHeaders(headers, "inherit")}</TableRow>
           </TableHead>
           <TableBody>
             {applications.map((application) => (
