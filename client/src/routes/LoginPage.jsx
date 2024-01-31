f9127962388defcb1ea77b98183d09c74904a5f9
Carlos Valeriano <carlosv.2064@gmail.com>
2024-02-01 00:20:17 +0100

    login page small fix

--- client/src/routes/LoginPage.jsx
+++ client/src/routes/LoginPage.jsx
@@ -11,33 +11,33 @@ function LoginPage() {
   const { mode } = useThemeContext();
   return (
     <div id="login-page">
-      <Container maxWidth="xs">
-        <Box
-          sx={{
-            marginTop: 16,
-            display: "flex",
-            flexDirection: "column",
-            alignItems: "center"
-          }}
-        >
-          <Box marginBottom={4}>
-            <img
-              src={logo}
-              alt="PoliLogo"
-              style={{
-                filter: mode === "dark" ? "brightness(0) invert(1)" : "invert(0)"
-              }}
-              height={100}
-            />
-          </Box>
-          <Avatar sx={{ m: 1, bgcolor: "primary.main" }}>
-            <LockOutlinedIcon />
-          </Avatar>
-          <Typography component="h1" variant="h5">
-            Welcome
-          </Typography>
-          <LoginButton />
+      <Container
+        maxWidth="xs"
+        sx={{
+          height: "80vh",
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+          justifyContent: "center"
+        }}
+      >
+        <Box marginBottom={4}>
+          <img
+            src={logo}
+            alt="PoliLogo"
+            style={{
+              filter: mode === "dark" ? "brightness(0) invert(1)" : "invert(0)"
+            }}
+            height={100}
+          />
         </Box>
+        <Avatar sx={{ m: 1, bgcolor: "primary.main" }}>
+          <LockOutlinedIcon />
+        </Avatar>
+        <Typography component="h1" variant="h5">
+          Welcome
+        </Typography>
+        <LoginButton />
       </Container>
     </div>
   );
